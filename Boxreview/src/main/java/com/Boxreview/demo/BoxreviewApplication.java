1dc2041f894d161bfe1695fe961e526344838332
Ema_Gonzalez <emanuelgonzalezg2@gmail.com>
2022-06-01 19:50:22 -0300

    Cree proyecto

---
+++ Boxreview/src/main/java/com/Boxreview/demo/BoxreviewApplication.java
@@ -0,0 +1,13 @@
+package com.Boxreview.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxreviewApplication.class, args);
+	}
+
+}
---
+++ Boxreview/src/test/java/com/Boxreview/demo/BoxreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Boxreview.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
