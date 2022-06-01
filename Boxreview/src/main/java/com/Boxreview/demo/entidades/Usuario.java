eaed88c363fb73d830259778fdc5745dda1f1c6d
GalixB <bryam.cesar18@gmail.com>
2022-06-01 20:31:21 -0300

    Cargue atributos en Usuario
    con sus getters y setters

--- Boxreview/src/main/java/com/Boxreview/demo/entidades/Usuario.java
+++ Boxreview/src/main/java/com/Boxreview/demo/entidades/Usuario.java
@@ -13,4 +13,40 @@ public class Usuario {
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
     private String nombre;
+    private String email;
+    private String contrasenia;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+    
+    
 }
