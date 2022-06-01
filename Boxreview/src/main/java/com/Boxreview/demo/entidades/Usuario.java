4f7783d3c9fedc7b03f6203421693c0ebb0b530a
GalixB <bryam.cesar18@gmail.com>
2022-06-01 20:25:59 -0300

    Cree paquete entidades

---
+++ Boxreview/src/main/java/com/Boxreview/demo/entidades/Usuario.java
@@ -0,0 +1,16 @@
+package com.Boxreview.demo.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    private String nombre;
+}
