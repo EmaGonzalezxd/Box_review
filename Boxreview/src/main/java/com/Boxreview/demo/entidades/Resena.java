4485e97dfdbcfd4d7e8cea9d8970b587790d9a88
Agustin Facundo Tavano <agustintavano7@gmail.com>
2022-06-02 20:32:18 -0300

    creo metodos getes y seters de la entidad reseña sin usar lombok

--- Boxreview/src/main/java/com/Boxreview/demo/entidades/Resena.java
+++ Boxreview/src/main/java/com/Boxreview/demo/entidades/Resena.java
@@ -1,6 +1,6 @@
 package com.Boxreview.demo.entidades;
 
-import lombok.Data; 
+//import lombok.Data; 
 import com.Boxreview.demo.enumerations.EnumCalificacion;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,7 +10,7 @@ import org.hibernate.annotations.GenericGenerator;
 
 
 @Entity
-@Data
+//@Data
 public class Resena {
     
     @Id
@@ -29,6 +29,59 @@ public class Resena {
     
     @OneToOne
     private Pelicula pelicula; 
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public EnumCalificacion getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(EnumCalificacion calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Pelicula getPelicula() {
+        return pelicula;
+    }
+
+    public void setPelicula(Pelicula pelicula) {
+        this.pelicula = pelicula;
+    }
+
+    @Override
+    public String toString() {
+        return "Resena{" + "id=" + id + ", titulo=" + titulo + ", comentario=" + comentario + ", calificacion=" + calificacion + ", usuario=" + usuario + ", pelicula=" + pelicula + '}';
+    }
     
     
 
--- Boxreview/src/main/java/com/Boxreview/demo/servicios/ResenaServicio.java
+++ Boxreview/src/main/java/com/Boxreview/demo/servicios/ResenaServicio.java
@@ -1,11 +1,11 @@
-
-
 package com.Boxreview.demo.servicios;
 
 import com.Boxreview.demo.enumerations.EnumCalificacion;
 import com.Boxreview.demo.entidades.Pelicula;
 import com.Boxreview.demo.entidades.Usuario;
 import com.Boxreview.demo.entidades.Resena;
+import com.Boxreview.demo.repositorios.ResenaRepositorio;
+import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,20 +13,43 @@ import org.springframework.stereotype.Service;
 @Service
 public class ResenaServicio {
 
-    @Autowired  
-    private ResenaRepositorio resenaRepo; 
-    
+    @Autowired
+    private ResenaRepositorio resenaRepo;
+
     @Transactional
-    public Resena crear(String id, String titulo, String comentario, 
-            EnumCalificacion enumCalificacion, Usuario usuario, Pelicula pelicula){
-        
-        Resena resena= new Resena(); 
+    public Resena crear(String id, String titulo, String comentario,
+            EnumCalificacion Calificacion, Usuario usuario, Pelicula pelicula) {
+
+        Resena resena = new Resena();
         resena.setUsuario(usuario);
         resena.setPelicula(pelicula);
         resena.setTitulo(titulo);
         resena.setComentario(comentario);
-        resena.setCalificacion(enumCalificacion);
+        resena.setCalificacion(Calificacion);
+
+        return resenaRepo.save(resena);
+    }
+
+    @Transactional
+    public void modificar(String id, Usuario usuario, Pelicula pelicula,
+            String titulo, String comentario, EnumCalificacion Calificacion) {
+
+        Optional<Resena> resp = resenaRepo.findById(id);
+
+        if (resp.isPresent()) {
+
+            Resena resena = new Resena();
+
+            resena.setPelicula(pelicula);
+            resena.setTitulo(titulo);
+            resena.setComentario(comentario);
+            resena.setCalificacion(Calificacion);
+            
+            resenaRepo.save(resena); 
+        } else {
+            throw new Exception; 
+        }
         
-        return resenaRepo.save(resena); 
+        
     }
 }
