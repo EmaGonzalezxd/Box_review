be9e4b92b372ee4a004e935cb5906c62472851ef
GalixB <bryam.cesar18@gmail.com>
2022-06-06 20:35:03 -0300

    Controladores actualizacion

--- Boxreview/src/main/java/com/Boxreview/demo/controladores/PortalControlador.java
+++ Boxreview/src/main/java/com/Boxreview/demo/controladores/PortalControlador.java
@@ -2,6 +2,7 @@ package com.Boxreview.demo.controladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,7 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PortalControlador {
 
     @GetMapping("/")
-    public String registro() {
+    public String index() {
+        return "index.html";
+    }
+    
+//    @GetMapping("/")
+//    public String registro() {
+//        return "registro.html";
+//    }
+    
+    @PostMapping("/")
+    public String registrar() {
         return "registro.html";
     }
 }
