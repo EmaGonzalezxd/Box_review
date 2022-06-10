37e4dc4fad7645552015698ac70687a8ac45d07f
JuanBlazquez0919 <106562428+JuanBlazquez0919@users.noreply.github.com>
2022-06-09 21:44:00 -0300

    fotos

---
+++ Boxreview/src/main/java/com/Boxreview/demo/servicios/FotoServicio.java
@@ -0,0 +1,70 @@
+
+package com.Boxreview.demo.servicios;
+
+import org.springframework.web.multipart.MultipartFile;
+import com.Boxreview.demo.entidades.Foto;
+import com.Boxreview.demo.repositorios.FotoRepositorio;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class FotoServicio {
+    
+    
+    @Autowired
+    private FotoRepositorio fotoRepositorio;
+    
+    public Foto guardar(MultipartFile archivo) throws Exception{
+        if (archivo != null){
+            try{    
+                Foto foto = new Foto();
+                foto.setMime(archivo.getContentType());
+                foto.setNombre(archivo.getName());
+                foto.setContenido(archivo.getBytes());
+            
+                return fotoRepositorio.save(foto);
+            }catch (Exception e){
+                throw new Exception(".");
+            }
+        }
+           return null;
+       }
+      
+    
+    public Foto actualizar(String idFoto, MultipartFile archivo) throws Exception{
+                if (archivo != null){
+            try{    
+                Foto foto = new Foto();
+                
+                if(idFoto != null){
+                    Optional<Foto> respuesta = fotoRepositorio.findById(idFoto);
+                    if (respuesta.isPresent()){
+                        foto = respuesta.get();
+                    }
+                }
+                
+                foto.setMime(archivo.getContentType());
+                foto.setNombre(archivo.getName());
+                foto.setContenido(archivo.getBytes());
+            
+                return fotoRepositorio.save(foto);
+            }catch (Exception e){
+                throw new Exception(".");
+            }
+        }
+           return null;
+    }
+    
+    
+    
+    
+    
+    
+        
+}
+
+    
+
