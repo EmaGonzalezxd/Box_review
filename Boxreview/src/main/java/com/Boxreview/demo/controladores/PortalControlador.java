150cf0fd415c77c155ee898aa3c21f7afbac1821
sebaperezb <99844742+sebaperezb@users.noreply.github.com>
2022-06-27 20:28:44 -0300

    BORRE VISTAS LOGIN Y REGISTRO, PERSISTE USUARIO

--- Boxreview/src/main/java/com/Boxreview/demo/controladores/PortalControlador.java
+++ Boxreview/src/main/java/com/Boxreview/demo/controladores/PortalControlador.java
@@ -1,12 +1,21 @@
 package com.Boxreview.demo.controladores;
 
+import com.Boxreview.demo.servicios.UsuarioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("")
 public class PortalControlador {
+    
+    @Autowired
+    private UsuarioServicio usuarioServicio;
 
     @GetMapping("/")
     public String inicio() {
@@ -23,4 +32,23 @@ public class PortalControlador {
         return "registro.html";
     }
 
+    @PostMapping("/registrar")
+    public String registrar(ModelMap modelo, @RequestParam MultipartFile foto, @RequestParam String nombre, @RequestParam String apellido, @RequestParam String email, @RequestParam String contrasenia) {
+
+        try {
+
+            usuarioServicio.crear(foto, nombre, apellido, email, contrasenia);
+            modelo.put("titulo", "Felicidades!");
+            modelo.put("descripcion", "Usuario registrado satisfactoriamente.");
+
+            return "index.html";
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+            modelo.put("error", ex.getMessage());
+            return "index.html";
+        }
+
+    }
+    
 }
